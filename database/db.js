93554baf6604084a395df2ce29ad4da7d219accf
P-STORE <114459299+p-store13@users.noreply.github.com>
2023-08-21 11:52:56 +0700

    HxcoderID

--- database/db.js
+++ database/db.js
@@ -4,16 +4,16 @@ var __path = process.cwd(),
      { color } = require(__path + '/lib/color.js')
 
 // Connection URL
-var url = 'https://alphabot-api.herokuapp.com';
+var url = 'https://hxcoderid.my.id';
 try {
-if(url == 'https://alphabot-api.herokuapp.com') throw console.log(color('Cek konfigurasi database, var url belum diisi','red'));
+if(url == 'https://hxcoderid.my.id') throw console.log(color('Cek konfigurasi database, var url belum diisi','red'));
 } catch (e) {
 	return;
 	}
 var db = monk(url);
 
 db.then(() => {
-  console.log(color('Connected correctly to server, Kuhong-API','green'))
+  console.log(color('Connected correctly to server, HxcoderID API','green'))
 })
 .catch ((e) => {
 	console.log(color('Error : '+ e +'\n\nGagal connect ke database, \ncek configurasi database apakah Connection URL sudah benar','red'))
